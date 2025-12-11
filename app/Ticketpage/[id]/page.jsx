d36e122b8808cec4c886fd38417b91f16ce400f3
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-12-11 21:57:10 +0900

    organize to folder structure

---
+++ app/Ticketpage/[id]/page.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const TickePage = () => {
+  return (
+    <div>
+      <h2>TickePage Offcial</h2>
+    </div>
+  )
+}
+
+export default TickePage
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Hello World ticketing application 2025</h1>
+    </div>
+  )
+}
+
+export default Dashboard
