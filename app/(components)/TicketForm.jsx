524ee1b0b8b906f392dc069fce29d213e3f49a3e
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-02-22 23:42:19 +0900

    ticket page creating new building

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -6,14 +6,25 @@ import useState from 'react'
 
 const TicketForm = () => {
 
-    const startingTicketData = {
-        title: "",
-        description: "",
-        priority: 1,
-        progress: 0,
-        status: "Not Started",
-        category: "Hardware Problem",
-    }
+  const handleChange = () => {
+    const value = e.target.value
+    const name = e.target.value
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+
+  }
+
+  const startingTicketData = {
+      title: "",
+      description: "",
+      priority: 1,
+      progress: 0,
+      status: "Not Started",
+      category: "Hardware Problem",
+  }
 
     const [formData, setFormData] = useState(startingTicketData)
 
@@ -21,6 +32,16 @@ const TicketForm = () => {
     <div className='flex justify-center'>
       <form action="">
         <h3>Create Your ticket</h3>
+
+        <label htmlFor="">Title</label>
+        <input
+          type="text"
+          id='title'
+          name='title'
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
       </form>
     </div>
   )
--- app/Ticketpage/[id]/page.jsx
+++ app/Ticketpage/[id]/page.jsx
@@ -1,10 +1,13 @@
 
+import TicketForm from '@/app/(components)/TicketForm'
 import React from 'react'
 
 const TickePage = () => {
   return (
     <div>
       <h2>TickePage Offcial</h2>
+
+      <TicketForm />
     </div>
   )
 }
